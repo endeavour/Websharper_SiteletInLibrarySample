0a2ae8182fb643976d863c4f641ab613ddcf4cf9
James Freiwirth <james@perfectshuffle.co.uk>
2013-02-24 16:30:51 +0000

    Added upload handler

--- LibraryWithSitelet/Main.fs
+++ LibraryWithSitelet/Main.fs
@@ -12,7 +12,41 @@ open PerfectShuffle.WebsharperExtensions
 type NestedAction =
 | Foo
 | Bar of string
+| Upload
 | Ignore // No page here, but we have to have a bijection...
+
+module UploadHandler =
+  open IntelliFactory.WebSharper.Sitelets
+  
+  let private extractFilesFromRequest inputFieldName (requestContext:System.Web.HttpRequest) =
+    let fileDict = requestContext.Files
+    
+    let files =
+      fileDict.AllKeys
+      |> Seq.filter (fun key -> key = inputFieldName)
+      |> Seq.map (fun key -> fileDict.Get(key))
+    
+    files
+
+  let UploadContentFactory inputFieldName (redirectUrlFactory:Context<'a> -> string) (fileHandler:System.Web.HttpPostedFile -> unit) =
+    
+    let content =
+      Content.CustomContent (fun ctx ->
+        let request = System.Web.HttpContext.Current.Request
+        let files = extractFilesFromRequest inputFieldName request
+        
+        files |> Seq.iter fileHandler
+        
+        let redirectUrl = redirectUrlFactory ctx
+
+        {
+          Status = Http.Status.Custom 301 (Some "Moved Permanently")
+          Headers = [Http.Header.Custom "Location" redirectUrl]
+          WriteBody = ignore
+        }
+        )
+
+    content  
 
 module NestedSite =
 
@@ -33,6 +67,7 @@ module NestedSite =
      match action with
      | Foo -> templateWrapper fooContent
      | Bar x -> templateWrapper barContent
+     | Upload -> UploadHandler.UploadContentFactory "file" (fun _ -> "/success") (fun file -> () (*Save to disk etc here...*))
      | Ignore -> failwith "Should never end up here"
 
     sitelet |> Sitelet.FilterAction (function Ignore -> false | _ -> true)
